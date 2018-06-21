b8c65924e66cc41a81a976f4c4c505c118672055
unknown <m.shanti@windowslive.com>
2018-06-21 09:55:55 +0300

    add and remove fields dynamiclly

--- controllers/questionsController.js
+++ controllers/questionsController.js
@@ -1,8 +1,6 @@
 var questionsController = angular.module('questionsController', []);
 
 questionsController.controller('questionAdd',function(questionsService,$scope){
-            console.log(1);
-
   $scope.submit = function(){
         console.log(QEdit.question);
 
@@ -24,7 +22,12 @@ questionsController.controller('questionDelete',function(questionsService,$state
 
 });
 
-questionsController.controller('questionEdit',function(questionsService,$scope,$stateParams){
+questionsController.controller('questionEdit',function(questionsService,$scope,$stateParams,$element,$compile){
+  $scope.AddContactTypeControl = function() {
+    var divElement = angular.element(document.querySelector('#contactTypeDiv'));
+    var appendHtml = $compile('<contact-type></contact-type>')($scope);
+    divElement.append(appendHtml);
+  }
   $scope.questionTypeOptions = ["Behavioral", "Opnion", "Welcome & Introduction"];
   $scope.answerTypeOptions = ["Multiple Choice - Single Choice", "Free Text"];
   $scope.choice = "false";
@@ -56,42 +59,74 @@ questionsController.controller('questionEdit',function(questionsService,$scope,$
 
 
 
-questionsController.controller('MainCtrl',function MainCtrl($scope) {
-  $scope.count = 0;
-});
 
-//Directive that returns an element which adds buttons on click which show an alert on click
-questionsController.directive("addbuttonsbutton", function(){
+questionsController.directive('contactType', function($compile, $rootScope, $templateRequest, $sce) {
   return {
     restrict: "E",
-    template: "<a href='' addbuttons>+ Add Option</a>"
+    scope: {},
+    link: function(scope, element, attrs) {
+
+      var templateUrl = $sce.getTrustedResourceUrl('view/removeInput.htm');
+      $templateRequest(templateUrl).then(function(template) {
+        var linkFn = $compile(template);
+        var htmlString = linkFn(scope);
+        element.append(htmlString);
+
+      }, function() {
+        // An error has occurred
+      });
+
+
+    },
+    controller: function($rootScope, $scope, $element) {
+      $scope.Delete = function() {
+        //remove element and also destoy the scope that element
+        $element.remove();
+        $scope.$destroy();
+        //$rootScope.controlCount--;
+      }
+    }
   }
 });
 
-//Directive for adding buttons on click that show an alert on click
-questionsController.directive("addbuttons", function($compile){
-  return function(scope, element, attrs){
-    element.bind("click", function(){
-      scope.count++;
-      angular.element(document.getElementById('space-for-buttons')).append($compile('<contact-type></contact-type>')(scope));
-    });
-  };
-});
 
-questionsController.directive("contactType", function($compile){
-  return {
-    templateUrl: 'view/removeInput.htm'
-  };
-});
+// questionsController.controller('MainCtrl',function MainCtrl($scope) {
+//   $scope.count = 0;
+// });
+
+// //Directive that returns an element which adds buttons on click which show an alert on click
+// questionsController.directive("addbuttonsbutton", function(){
+//   return {
+//     restrict: "E",
+//     template: "<a href='' addbuttons>+ Add Option</a>",
+//   }
+// });
+
+// //Directive for adding buttons on click that show an alert on click
+// questionsController.directive("addbuttons", function($compile){
+//   return function(scope, element, attrs){
+//     element.bind("click", function(){
+//       scope.count++;
+//       angular.element(document.getElementById('space-for-buttons')).append($compile('<contact-type></contact-type>')(scope));
+//     });
+//   };
+// });
+
+// questionsController.directive("contactType", function($compile){
+//   return {
+//     templateUrl: 'view/removeInput.htm'
+//   };
+// });
 
 
-questionsController.directive('removeOnClick', function() {
-    return {
-        link: function(scope, elt, attrs) {
-            scope.remove = function() {
-                elt.html('');
-            };
-        }
-    }
-});
+// questionsController.directive('removeOnClick', function() {
+//     return {
+//         link: function(scope, elt, attrs) {
+//             scope.remove = function() {
+//                 elt.html('');
+//             };
+//         }
+//     }
+// });
+
 
